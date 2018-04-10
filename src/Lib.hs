e8c8a7caf986e2288c5dcbae8a1e614390b93693
Carlos Gustavo Ruiz <atmantree@gmail.com>
2018-04-10 11:14:50 -0500

    get content from url

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib where
 
+import Network.Wreq
+import Control.Lens
+
 showMsg :: IO ()
-showMsg = putStrLn "getting .gitignore" 
+showMsg = do
+            r <- get "https://raw.githubusercontent.com/github/gitignore/master/Haskell.gitignore"
+            print $ r ^. responseBody
